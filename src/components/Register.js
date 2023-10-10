03ab30b033d4163f3c22662f344ae378310a58d4
Ishkhan <ioterzian@gmail.com>
2023-10-10 18:15:26 -0400

    test

--- src/components/Register.js
+++ src/components/Register.js
@@ -22,7 +22,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      await axios.post("http://localhost:3001/register", {
+      await axios.post("https://night-in.xyz/register", {
         username: formData.username,
         email: formData.email,
         password: formData.password,
