ea70f39de36eee1e646ccd639b9991a7d0a419b2
Ishkhan <ioterzian@gmail.com>
2023-10-11 02:10:43 -0400

    updates

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+const fs = require('fs');
 require('dotenv').config();
 
 const db_connection = mysql.createConnection({
@@ -6,7 +7,10 @@ const db_connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: {
+        ca: fs.readFileSync('./DigiCertGlobalRootCA.crt.pem')
+    }
 });
 
 module.exports = db_connection;
