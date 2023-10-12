65c3d771467d79b451e4bdee2d1920cf606bcca2
Ishkhan <ioterzian@gmail.com>
2023-10-12 02:27:54 -0400

    t

--- backend/db.js
+++ backend/db.js
@@ -8,6 +8,7 @@ const db_connection = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     port: process.env.DB_PORT,
+    encrypt: true,
 });
 
 module.exports = db_connection;
