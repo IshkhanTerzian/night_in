914241eb057efde547a779b06483b9e07f5ae3ea
Ishkhan <ioterzian@gmail.com>
2023-10-13 14:20:35 -0400

    test

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 //import axios from "axios";
 //import config from "../config.json";
